e474a1b4811fe004998bf7e633dcfd8b585d5674
Taylor <bKuzzcs@gmail.com>
2022-03-01 13:59:34 -0700

    commit tree fc1d26ce91680f6e27271cfcc5f02405a6bb9126

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(nameArray, type) {
+    let thankYou = []; 
+    for (let i = 0; i <nameArray.length; i++) {
+        thankYou.push(`Thank you, ${nameArray[i]}, for the wonderful ${type} gift!`);
+    }
+    return thankYou;
+}
+
+function countDown() {
+
+var countDown = 11; 
+    while (countDown > 0) {
+        countDown--
+        console.log(countDown);
+    }
+}
